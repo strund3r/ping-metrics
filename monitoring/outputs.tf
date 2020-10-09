5d4d271ace49891bccd6dd3beeea08b7aa6db452
Franzwagner Ternus <franzwagner.str@gmail.com>
2020-10-08 22:23:11 -0300

    Remove unnecessary outputs

--- monitoring/outputs.tf
+++ monitoring/outputs.tf
@@ -3,16 +3,6 @@ output "lb_ip_prometheus" {
   value       = kubernetes_service.prometheus.load_balancer_ingress[0].hostname
 }
 
-output "lb_ip_metrics" {
-  description = "Show kube-state-metrics Load Balancer External IP"
-  value       = kubernetes_service.kube_state_metrics.load_balancer_ingress[0].hostname
-}
-
-output "lb_ip_cadvisor" {
-  description = "Show cAdvisor Load Balancer External IP"
-  value       = kubernetes_service.cadvisor.load_balancer_ingress[0].hostname
-}
-
 output "lb_ip_grafana" {
   description = "Show Grafana Loab Balancer External IP"
   value       = kubernetes_service.grafana.load_balancer_ingress[0].hostname
