1342475e5f6cd983da5a60497cd8a3b2d8848c16
Franzwagner Ternus <franzwagner.str@gmail.com>
2020-10-08 22:31:51 -0300

    Rewrite file to a canonical format and style

--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,9 +1,9 @@
 output "eks_s3_bucket_domain_name" {
-    description = "Print EKS S3 bucket domain name"
-    value = aws_s3_bucket.eks_state.bucket_domain_name
+  description = "Print EKS S3 bucket domain name"
+  value       = aws_s3_bucket.eks_state.bucket_domain_name
 }
 
 output "monitoring_s3_bucket_domain_name" {
-    description = "Print monitoring S3 bucket domain name"
-    value = aws_s3_bucket.monitoring_state.bucket_domain_name
+  description = "Print monitoring S3 bucket domain name"
+  value       = aws_s3_bucket.monitoring_state.bucket_domain_name
 }
