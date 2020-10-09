7e0588429461d89157a08770e8856dc1fa5c1a7d
Franzwagner Ternus <franzwagner.str@gmail.com>
2020-10-08 22:22:42 -0300

    Add Grafana and Time providers

--- monitoring/provider.tf
+++ monitoring/provider.tf
@@ -4,7 +4,22 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 1.13"
     }
+    grafana = {
+      source  = "grafana/grafana"
+      version = "~> 1.5"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.6"
+    }
   }
 }
 
 provider "kubernetes" {}
+
+provider "grafana" {
+  url  = "http://${kubernetes_service.grafana.load_balancer_ingress[0].hostname}:3000"
+  auth = "admin:admin" #var.grafana_auth
+}
+
+provider "time" {}
