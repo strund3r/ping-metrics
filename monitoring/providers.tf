53c6c057c77f0ae4075ab428b82f25b73c702e91
Franzwagner Ternus <franzwagner.str@gmail.com>
2020-10-15 18:27:30 -0300

    Added missing providers and added AWS related variables

--- monitoring/providers.tf
+++ monitoring/providers.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 1.13"
@@ -12,14 +16,24 @@ terraform {
       source  = "hashicorp/time"
       version = "~> 0.6"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.1"
+    }
   }
+}
+
+provider "aws" {
+  region                  = var.aws_region
+  shared_credentials_file = var.aws_credential
+  profile                 = var.aws_profile
 }
 
 provider "kubernetes" {}
 
 provider "grafana" {
   url  = "http://${kubernetes_service.grafana.load_balancer_ingress[0].hostname}:3000"
-  auth = "admin:admin" #var.grafana_auth
+  auth = var.grafana_auth
 }
 
 provider "time" {}
--- monitoring/variables.tf
+++ monitoring/variables.tf
@@ -3,3 +3,32 @@ variable "k8s_namespace" {
   type        = string
   default     = "monitoring"
 }
+
+variable "aws_profile" {
+  description = "Name of AWS profile in AWS shared credentials file or AWS shared configuration file."
+  type        = string
+  default     = "default"
+}
+
+variable "aws_region" {
+  description = "AWS region."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_credential" {
+  description = "Path to the AWS shared credentials file."
+  type        = string
+  default     = "~/.aws/credentials"
+}
+
+variable "eks_cluster_name" {
+  description = "Name of the EKS cluster."
+  type        = string
+  default     = "eks-cluster"
+}
+
+variable "grafana_auth" {
+  description = "Grafana username and password (Usage: user:pass)"
+  type        = string
+}
